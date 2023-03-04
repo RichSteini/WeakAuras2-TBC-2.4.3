9826dff9ac7073cf45a9111b2ecec9ff829cef43
Richard Steininger <richi.steininger@hotmail.com>
2023-03-04 14:57:03 +0100

    Fix auto-clone aura trigger setting

--- WeakAuras/BuffTrigger2.lua
+++ WeakAuras/BuffTrigger2.lua
@@ -1204,7 +1204,7 @@ local function UpdateTriggerState(time, id, triggernum)
 
       local usedCloneIds = {};
       for index, auraData in ipairs(auraDatas) do
-        local cloneId = (auraData.GUID or auraData.unit or "unknown") .. " " .. auraData.spellId
+        local cloneId = (auraData.GUID or auraData.unit or "unknown") .. " " .. auraData.name
         if usedCloneIds[cloneId] then
           usedCloneIds[cloneId] = usedCloneIds[cloneId] + 1
           cloneId = cloneId .. usedCloneIds[cloneId]
